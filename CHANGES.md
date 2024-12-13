862ac9b51cb91ab22bc604db65d1d1c043920cf0
Woomeeme <2968606874@qq.com>
2024-12-13 14:17:29 +0800

    create cpdb-libs (2.0~b5-1) unstable

---
+++ CHANGES.md
@@ -0,0 +1,199 @@
+# CHANGES - Common Print Dialog Backends - Libraries - v2.0b5 - 2023-08-02
+
+## CHANGES IN V2.0b5 (2nd August 2023)
+
+ - Removed browsing for backends via file system
+   The frontend should only shout into the D-Bus to find out which
+   backends are available and to communicate with them. Depending on
+   the way (for example sandboxed packaging, like Snap) how the
+   frontend and backand are installed the frontend cannot access the
+   host's or the backend's file systems (PR #27).
+
+ - Limit scanned string length in `scanf()`/`fscanf()` functions
+   cpdb-libs uses the `fscanf()` and `scanf()` functions to parse
+   command lines and configuration files, dropping the read string
+   components into fixed-length buffers, but does not limit the length
+   of the strings to be read by `fscanf()` and `scanf()` causing
+   buffer overflows when a string is longer than 1023 characters
+   (CVE-2023-34095).
+
+ - Fixed memory bugs leading to leaks and crashes (PR #26)
+
+ - Build system: Removed unnecessary lines in `tools/Makefile.am`
+   Removed the `TESTdir` and `TEST_SCRIPTS` entries in
+   `tools/Makefile.am`.  They are not needed and let `make install`
+   try to install `run-tests.sh` in the source directory, where it
+   already is, causing an error.
+
+
+## CHANGES IN V2.0b4 (20th March 2023)
+
+ - Added test script for `make test`/`make check`
+   The script tools/run-tests.sh runs the `cpdb-text-frontend` text mode
+   example frontend and stops it by supplying "stop" to its standard
+   input.
+
+ - Allow changing the backend info directory via env variable
+   To make it possible to test backends which are not installed into
+   the system, one can now set the environment variable
+   CPDB_BACKEND_INFO_DIR to the directory where the backend info file
+   for the backend is, for example in its source tree.
+
+ - Install sample frontend with `make install`
+   We use the sample frontend `cpdb-text-frontend` for several tests now,
+   especially "make check" and also the autopkgtests in the
+   Debian/Ubuntu packages. They are also useful for backend developers
+   for manual testing.
+
+ - Renamed develping/debug tools
+   As we install the development and debugging tools now, they should
+   be more easily identifiable as part of CPDB. Therefore they get
+   `cpdb-`-prefixed names.
+
+ - `cpdb-text-frontend`: Use larger and more easily adjustable string
+   buffers
+
+ - Fixed segfault in the frontend library
+   `cpdbResurrectPrinterFromFile()`, when called with an invalid file
+   name, caused a crash.
+
+
+## CHANGES IN V2.0b3 (20th February 2023)
+
+- Added functions to fetch all printer strings translations (PR #23)
+  * Added `cpdbGetAllTranslations()` to synchronously fetch all
+    printer string translations
+  * Added `cpdbAcquireTranslations()` to asychronously fetch them.
+  * Removed `get-human-readable-option`/`choice-name` methods
+  * Removed `cpdb_async_obj_t` from `cpdb-frontend.h` as that is meant
+    for internal use.
+
+
+## CHANGES IN V2.0b2 (13th February 2023)
+
+- Options groups: To allow better structuring of options in print
+  dialogs, common options are categorized in groups, like media, print
+  quality, color, finishing, ... This can be primarily done by the
+  backends but the frontend library can do fallback/default
+  assignments for options not assigned by the backend.
+
+- Many print dialogs have a "Color" option group (usually shown on one
+  tab), so also have one in cpdb-libs to match with the dialogs and
+  more easily map the options into the dialogs.
+
+- Add macros for new options and choices, also add "Color" group
+
+- Synchronous printer fetching upon backend activation (PR #21) Made
+  `cpdbConnectToDbus()` wait until all backends activate
+
+- Backends will automatically signal any printer updates instead of
+  the frontend having to manually ask them (PR #21)
+
+- Add `printer-state-changed` signal (PR #21)
+  * Changed function callback type definition for printer updates
+  * Added callback to frontends for changes in printer state
+
+- Translation support: Translations of option, choice, and group names
+  are now supported, not only English human-readable strings. And
+  Translations can be provided by the backends (also polling them from
+  the print service) and by the frontend library.
+
+- Use autopoint instead of gettextize
+
+- Enable reconnecting to dbus (PR #14)
+
+- Debug logging: Now backends forward their log messages to the
+  frontend library for easier debugging.
+
+- Use <glib/gi18n.h> instead of redefining macros (Issue #20)
+
+- Add functions to free objects (PR #15)
+
+- Remove hardcoded paths and follow XDG base dir specs (PR #14)
+
+- Added javadoc comments to function declarations (PR #21)
+
+- Build system: Let "make dist" also create .tar.bz2 and .tar.xz
+
+- Add the dependency on libdbus to README.md
+  libdbus-1-dev is needed to configure pkg-config variables for
+  backends
+
+- COPYING: Added Priydarshi Singh
+
+
+## CHANGES IN V2.0b1 (11th December 2022)
+
+- Added interfaces to get human readable option and settings names
+    
+  Print attributes/options and their choices are usually defined in a
+  machine-readable form which is more made for easy typing in a
+  command line, not too long, no special characters, always in English
+  and human-readable form for GUI (print dialogs), more verbose for
+  easier understanding, with spaces and other special characters,
+  translated, ...
+
+  Older backends without human-readable strings can still be used. In
+  such a case it is recommended that the dialog does either its own
+  conversion or simply shows the machine-readable string as a last
+  mean.
+
+- Added get_media_size() function to retrieve media dimensions for a
+  given "media" option value
+
+- Support for media sizes to have multiple margin variants (like
+  standard and borderless)
+
+- Support for configurable user and system-wide default printers
+
+- Acquire printer details asynchronously (non blocking)
+
+- Made cpdb-libs completely CUPS-neutral
+
+  Removed CUPS-specific functions from the frontend library functions
+  and the dependency on libcups, renamed CUPS-base function and signal
+  names
+
+- Use "const" qualifiers on suitable function parameters
+
+- DBG_LOG now includes error-message
+
+- Renamed all API functions, data types and constants
+    
+  To make sure that the resources of libcpdb and libcpdb-frontend do
+  not conflict with the ones of any other library used by a frontend
+  or backend created with CPDB, all functions, data types, and
+  constants of CPDB got renamed to be unique to CPDB.
+    
+  Here we follow the rules of CUPS and cups-filters (to get unique
+  rules for all libraries by OpenPrinting): API functions are in
+  camelCase and with "cpdb" prefix, data types all-lowercase, with '_'
+  as word separator, and constants are all-uppercase, also with '_' as
+  word separator, and with "CPDB_" prefix.
+
+- Renamed and re-organized source files to make all more
+  standards-conforming and naming more consistent.
+    
+- All headers go to /usr/include/cpdb now: Base API headers cpdb.h and
+  cpdb-frontend.h, interface headers (and also part of the API)
+  backend-interface.h and frontend-interface.h, and the convenience
+  header files backend.h and frontend.h (include exactly the headers
+  needed).
+    
+- Bumped soname of the libraries to 2.
+    
+- Check settings pointer for NULL before freeing it
+
+- NULL check on input parameters for many functions
+
+- Fixed incompatibility with newer versions of glib()
+
+  glib.h cannot get included inside 'extern "C" { ... }'
+
+- Corrected AC_INIT() in configure.ac: Bug report destination,
+  directory for "make dist".
+
+- README.md: Fixed typos and updated usage instructions
+
+- Updated .gitignore
+
---
+++ cpdb/cpdb.c
@@ -0,0 +1,378 @@
+#include "cpdb.h"
+
+/**
+ * If CPDB has been initialized
+ */
+volatile gboolean initialized = FALSE;
+
+/**
+ * Table matching common IPP options prefix to groups
+ */
+const char *cpdbGroupTable[][2] = {
+
+    {CPDB_OPTION_COPIES,                        CPDB_GROUP_COPIES},
+    {CPDB_OPTION_COLLATE,                       CPDB_GROUP_COPIES}, 
+    {CPDB_OPTION_COPIES_SUPPORTED,              CPDB_GROUP_COPIES},
+
+    {CPDB_OPTION_MEDIA,                         CPDB_GROUP_MEDIA},
+    {CPDB_OPTION_MEDIA_TYPE,                    CPDB_GROUP_MEDIA},
+
+    {CPDB_OPTION_SIDES,                         CPDB_GROUP_PAGE_MGMT},
+    {CPDB_OPTION_MIRROR,                        CPDB_GROUP_PAGE_MGMT},
+    {CPDB_OPTION_BOOKLET,                       CPDB_GROUP_PAGE_MGMT},
+    {CPDB_OPTION_PAGE_SET,                      CPDB_GROUP_PAGE_MGMT},
+    {CPDB_OPTION_NUMBER_UP,                     CPDB_GROUP_PAGE_MGMT},
+    {CPDB_OPTION_PAGE_BORDER,                   CPDB_GROUP_PAGE_MGMT},
+    {CPDB_OPTION_PAGE_RANGES,                   CPDB_GROUP_PAGE_MGMT},
+    {CPDB_OPTION_ORIENTATION,                   CPDB_GROUP_PAGE_MGMT},
+
+    {CPDB_OPTION_POSITION,                      CPDB_GROUP_SCALING},
+    {CPDB_OPTION_PRINT_SCALING,                 CPDB_GROUP_SCALING},
+    {CPDB_OPTION_FIDELITY,                      CPDB_GROUP_SCALING},
+
+    {CPDB_OPTION_COLOR_MODE,                    CPDB_GROUP_COLOR},
+
+    {CPDB_OPTION_PRINT_QUALITY,                 CPDB_GROUP_QUALITY},
+    {CPDB_OPTION_RESOLUTION,                    CPDB_GROUP_QUALITY},
+    
+    {CPDB_OPTION_FINISHINGS,                    CPDB_GROUP_FINISHINGS},
+    {CPDB_OPTION_OUTPUT_BIN,                    CPDB_GROUP_FINISHINGS},
+    {CPDB_OPTION_PAGE_DELIVERY,                 CPDB_GROUP_FINISHINGS},
+
+    {CPDB_OPTION_JOB_NAME,                      CPDB_GROUP_JOB_MGMT},
+    {CPDB_OPTION_JOB_SHEETS,                    CPDB_GROUP_JOB_MGMT},
+    {CPDB_OPTION_JOB_PRIORITY,                  CPDB_GROUP_JOB_MGMT},
+    {CPDB_OPTION_BILLING_INFO,                  CPDB_GROUP_JOB_MGMT},
+    {CPDB_OPTION_JOB_HOLD_UNTIL,                CPDB_GROUP_JOB_MGMT}
+    
+};
+
+static void cpdbDebugLog(CpdbDebugLevel msg_lvl, const char *msg);
+
+
+void cpdbInit()
+{
+    if (!initialized)
+    {
+        bindtextdomain(CPDB_GETTEXT_PACKAGE, CPDB_LOCALEDIR);
+        initialized = TRUE;
+    }
+}
+
+char **cpdbNewCStringArray(int num_elems)
+{
+    return malloc(sizeof(char *) * num_elems);
+}
+
+gboolean cpdbGetBoolean(const char *g)
+{
+    if (!g)
+        return FALSE;
+
+    if (g_str_equal(g, "true"))
+        return TRUE;
+
+    return FALSE;
+}
+
+char *cpdbConcat(const char *s1, const char *s2)
+{
+    if (s1 == NULL)
+        return cpdbGetStringCopy(s2);
+    if (s2 == NULL)
+        return cpdbGetStringCopy(s1);
+
+    char *s = malloc(strlen(s1) + strlen(s2) + 1);
+    sprintf(s, "%s%s", s1, s2);
+    return s;
+}
+
+char *cpdbConcatSep(const char *s1, const char *s2)
+{
+    char *s = malloc(strlen(s1) + strlen(s2) + 2);
+    sprintf(s, "%s#%s", s1, s2);
+    return s;
+}
+
+char *cpdbConcatPath(const char *s1, const char *s2)
+{
+    char *s = malloc(strlen(s1) + strlen(s2) + 2);
+    if (s1[strlen(s1) - 1] == '/')
+        sprintf(s, "%s%s", s1, s2);
+    else
+        sprintf(s, "%s/%s", s1, s2);
+    return s;
+}
+
+char *cpdbGetStringCopy(const char *str)
+{
+    if (str == NULL)
+        return NULL;
+    char *s = malloc(sizeof(char) * (strlen(str) + 1));
+    strcpy(s, str);
+    return s;
+}
+
+void cpdbUnpackStringArray(GVariant *variant, int num_val, char ***val)
+{
+    GVariantIter *iter;
+
+    if (!num_val)
+    {
+        printf("No supported values found.\n");
+        *val = NULL;
+        return;
+    }
+    char **array = (char **)malloc(sizeof(char *) * num_val);
+
+    g_variant_get(variant, "a(s)", &iter);
+
+    int i = 0;
+    char *str;
+    for (i = 0; i < num_val; i++)
+    {
+        g_variant_iter_loop(iter, "(s)", &str);
+        array[i] = cpdbGetStringCopy(str);
+        printf(" %s\n", str);
+    }
+    *val = array;
+}
+
+GVariant *cpdbPackStringArray(int num_val, char **val)
+{
+    GVariantBuilder *builder;
+    GVariant *values;
+    builder = g_variant_builder_new(G_VARIANT_TYPE("a(s)"));
+    for (int i = 0; i < num_val; i++)
+    {
+        g_variant_builder_add(builder, "(s)", val[i]);
+    }
+
+    if (num_val == 0)
+        g_variant_builder_add(builder, "(s)", "NA");
+
+    values = g_variant_builder_end(builder);
+    return values;
+}
+
+GVariant *cpdbPackMediaArray(int num_val, int (*margins)[4])
+{
+	GVariantBuilder *builder;
+	GVariant *values;
+	builder = g_variant_builder_new(G_VARIANT_TYPE("a(iiii)"));
+	for (int i = 0; i < num_val; i++)
+	{
+		g_variant_builder_add(builder, "(iiii)", 
+							  margins[i][0], margins[i][1], margins[i][2], margins[i][3]);
+	}
+	
+	values = g_variant_new("a(iiii)", builder);
+	return values;
+}
+
+char *cpdbGetAbsolutePath(const char *file_path)
+{
+    if (!file_path)
+        return NULL;
+
+    if (file_path[0] == '/')
+        return cpdbGetStringCopy(file_path);
+
+    if (file_path[0] == '~')
+    {
+        struct passwd *passwdEnt = getpwuid(getuid());
+        char *home = passwdEnt->pw_dir;
+	if (home == NULL)
+	  return NULL;
+	if (file_path[1] != '/' && file_path[1] != '\0')
+	  return NULL;
+	char *fp = malloc(sizeof(char) * (strlen(home) + strlen(file_path)));
+	if (fp == NULL)
+	  return NULL;
+        sprintf(fp, "%s%s", home, file_path + 1);
+        return fp;
+    }
+    char *cwd = getcwd(NULL, 0);
+    if (cwd == NULL)
+      return NULL;
+    char *fp = malloc(sizeof(char) * (strlen(cwd) + strlen(file_path) + 2));
+    if (fp == NULL)
+      return NULL;
+    sprintf(fp, "%s/%s", cwd, file_path);
+    free(cwd);
+    printf("%s\n", fp);
+    return fp;
+}
+char *cpdbExtractFileName(const char *file_path)
+{
+    if (!file_path)
+        return NULL;
+
+    char *file_name_ptr = (char *)file_path;
+
+    char *x = (char *)file_path;
+    char c = *x;
+    while (c)
+    {
+        if (c == '/')
+            file_name_ptr = x;
+
+        x++;
+        c = *x;
+    }
+    return file_name_ptr;
+}
+
+char *cpdbGetUserConfDir()
+{
+    char *config_dir = NULL, *env_xch, *env_home;
+
+    if (env_xch = getenv("XDG_CONFIG_HOME"))
+        config_dir = cpdbConcatPath(env_xch, "cpdb");
+    else if (env_home = getenv("HOME"))
+        config_dir = cpdbConcatPath(env_home, ".config/cpdb");
+
+    if (config_dir && (access(config_dir, R_OK) == 0 || mkdir(config_dir, CPDB_USRCONFDIR_PERM) == 0))
+        return config_dir;
+
+    return NULL;
+}
+
+char *cpdbGetSysConfDir()
+{
+    char *config_dir = NULL, *env_xcd, *path;
+
+#ifdef CPDB_SYSCONFDIR
+    config_dir = cpdbConcatPath(CPDB_SYSCONFDIR, "cpdb");
+    if (access(config_dir, R_OK) == 0 || mkdir(config_dir, CPDB_SYSCONFDIR_PERM) == 0)
+        return config_dir;
+#endif
+
+    if (env_xcd = getenv("XDG_CONFIG_DIRS"))
+    {
+        env_xcd = cpdbGetStringCopy(env_xcd);
+        path = strtok(env_xcd, ":");
+        while (path != NULL)
+        {
+            config_dir = cpdbConcatPath(CPDB_SYSCONFDIR, "cpdb");
+            if (access(config_dir, R_OK) == 0 || mkdir(config_dir, CPDB_SYSCONFDIR_PERM) == 0)
+            {
+                free(env_xcd);
+                return config_dir;
+            }
+
+            free(config_dir);
+            path = strtok(NULL, ":");
+        }
+        free(env_xcd);
+    }
+        
+    config_dir = "/etc/cpdb";
+    if (access(config_dir, R_OK) == 0 || mkdir(config_dir, CPDB_SYSCONFDIR_PERM) == 0)
+        return config_dir;
+
+    return NULL;
+}
+
+char *cpdbGetGroup(const char *option_name)
+{
+    if (option_name == NULL)
+    {
+        cpdbDebugLog(CPDB_DEBUG_LEVEL_WARN, "Invalid params: cpdbGetCommonGroup()\n");
+        return NULL;
+    }
+
+    int num_group = sizeof(cpdbGroupTable) / sizeof(cpdbGroupTable[0]);
+    for (int i = 0; i < num_group; i++)
+    {
+        if (strncmp(option_name, cpdbGroupTable[i][0], strlen(cpdbGroupTable[i][0])) == 0)
+            return cpdbGetStringCopy(cpdbGroupTable[i][1]);
+    }
+
+    return cpdbGetStringCopy(CPDB_GROUP_ADVANCED);
+}
+
+char *cpdbGetGroupTranslation2(const char *group_name, const char *lang)
+{
+    setenv("LANGUAGE", lang, 1);
+
+    {
+        extern int  _nl_msg_cat_cntr;
+        ++_nl_msg_cat_cntr;
+    }
+
+    return cpdbGetStringCopy(_(group_name));
+}
+
+static void cpdbDebugLog(CpdbDebugLevel msg_lvl, const char *msg)
+{
+    FILE *log_file = NULL, *out;
+    char *env_cdl, *env_cdlf;
+    CpdbDebugLevel dbg_lvl;
+
+    if (msg == NULL)
+        return;
+
+    dbg_lvl = CPDB_DEBUG_LEVEL_ERROR;
+    if (env_cdl = getenv(CPDB_DEBUG_LEVEL))
+    {
+		if (strncasecmp(env_cdl, "debug", 5) == 0)
+			dbg_lvl = CPDB_DEBUG_LEVEL_DEBUG;
+        else if (strncasecmp(env_cdl, "info", 4) == 0)
+            dbg_lvl = CPDB_DEBUG_LEVEL_INFO;
+        else if (strncasecmp(env_cdl, "warn", 4) == 0)
+            dbg_lvl = CPDB_DEBUG_LEVEL_WARN;
+    }
+    if (msg_lvl < dbg_lvl)
+		return;
+    
+    out = stderr;
+    if (env_cdlf = getenv(CPDB_DEBUG_LOGFILE))
+    {
+        if ((log_file = fopen(env_cdlf, "a")) != NULL)
+			out = log_file;
+	}
+
+	switch(msg_lvl)
+	{
+		case CPDB_DEBUG_LEVEL_DEBUG:
+			fprintf(out, "[Debug] %s", msg);
+			break;
+		case CPDB_DEBUG_LEVEL_INFO:
+			fprintf(out, "[Info] %s", msg);
+			break;
+		case CPDB_DEBUG_LEVEL_WARN:
+			fprintf(out, "[Warn] %s", msg);
+			break;
+		case CPDB_DEBUG_LEVEL_ERROR:
+			fprintf(out, "[Error] %s", msg);
+			break;
+	}
+}
+
+void cpdbFDebugPrintf(CpdbDebugLevel msg_lvl, const char *fmt, ...)
+{
+    va_list argptr;
+	char buf[CPDB_BSIZE], msg[CPDB_BSIZE + 12];
+	
+	va_start(argptr, fmt);
+	vsnprintf(buf, sizeof(buf), fmt, argptr);
+    snprintf(msg, sizeof(msg), "[Frontend] %s", buf);
+	va_end(argptr);
+	
+	cpdbDebugLog(msg_lvl, msg);
+}
+
+void cpdbBDebugPrintf(CpdbDebugLevel msg_lvl, const char *backend_name,
+                        const char *fmt, ...)
+{
+    va_list argptr;
+	char buf[CPDB_BSIZE], msg[CPDB_BSIZE + 12];
+	
+	va_start(argptr, fmt);
+	vsnprintf(buf, sizeof(buf), fmt, argptr);
+    snprintf(msg, sizeof(msg), "[Backend %s] %s", backend_name, buf);
+	va_end(argptr);
+	
+	cpdbDebugLog(msg_lvl, msg);
+}
